fef45bb87ec5021bcf3d1c08a868b2733f630731
Agha Safarzade <77771405+agha-safarzada@users.noreply.github.com>
2024-05-27 11:13:59 +0200

    Update ProfileFormPage.java

--- tests/src/test/java/mytest/ProfileFormPage.java
+++ tests/src/test/java/mytest/ProfileFormPage.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebDriver;
 public class ProfileFormPage extends BasePage {
 
     private By usernameBy = By.name("username");
-    private By radioButtonBy = By.id("radio-button-id"); // Adjust ID accordingly
-    private By checkBoxBy = By.id("check-box-id"); // Adjust ID accordingly
+    private By radioButtonBy = By.id("radio-button-id"); 
+    private By checkBoxBy = By.id("check-box-id"); 
     private By saveButtonBy = By.cssSelector(".save-button");
 
     public ProfileFormPage(WebDriver driver) {
         super(driver);
-        this.driver.get("https://www.trendyol.com/user/profile"); // Adjust URL accordingly
+        this.driver.get("https://www.trendyol.com/user/profile"); 
     }
 
     public void fillForm(String username, boolean selectRadio, boolean checkCheckbox) {
